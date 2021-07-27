da211809a8df66fdaf02611631d21981511dfd34
Hantonny Korrea <hantonny.20@gmail.com>
2021-07-26 22:51:31 -0400

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
         <title>Create Next App</title>
       </Head>
       <main>
