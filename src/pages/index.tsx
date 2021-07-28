43ab7d95550f3e45c10d720f4fb19fd664e6f323
Hantonny Korrea <hantonny.20@gmail.com>
2021-07-27 22:33:34 -0400

    Sorteio de 1 numero por intervalo determinado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,24 @@
+import React, { useState } from 'react';
+
 export default function Home() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [number3, setNumber3] = useState('');
+  const [result, setResult] = useState(0);
+
+  const handleNumber1 = (number1) => {
+    setNumber1(number1.target.value);
+  }
+  const handleNumber2 = (number2) => {
+    setNumber2(number2.target.value);
+  }
+  const handleNumber3 = (number3) => {
+    setNumber3(number3.target.value);
+  }
+  const handleResult = () => {
+    const interacao = Math.floor(Math.random() * (parseInt(number3) - parseInt(number2) + 1)) + parseInt(number2);
+    setResult(interacao)
+  }
   return (
     <div className="container-fluid">
       <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#fd7c59' }}>
@@ -15,17 +35,18 @@ export default function Home() {
                   <h5 className="card-title">Sortear números</h5>
                   <p className="card-text text-center">Crie o sorteio de números facilmente utilizando essa opção, você poderá salvar o sorteio e compartilhar o link com outras pessoas.</p>
                   <p className="card-text"><small className="text-muted">O resultado terá quantos números?</small></p>
-                  <input type="number" className="form-control" id="exampleFormControlInput1"></input>
+                  <input type="number" className="form-control" id="numberquant" name="number1" onChange={(number1) => handleNumber1(number1)} value={number1}></input>
                   <p className="card-text mt-2"><small className="text-muted">Entre</small></p>
-                  <input type="number" className="form-control" id="exampleFormControlInput1"></input>
+                  <input type="number" className="form-control" id="number2" name="number2" onChange={(number2) => handleNumber2(number2)} value={number2}></input>
                   <p className="card-text mt-2"><small className="text-muted">e</small></p>
-                  <input type="number" className="form-control" id="exampleFormControlInput1"></input>
+                  <input type="number" className="form-control" id="number3" name="number3" onChange={(number3) => handleNumber3(number3)} value={number3}></input>
                   <div className="d-grid gap-2 d-md-flex justify-content-md-center mt-3">
-                    <button type="button" className="btn text-white fw-bold" style={{ backgroundColor: '#fd7c59' }}>TESTAR SORTEIO </button>
+                    <button type="button" className="btn text-white fw-bold" style={{ backgroundColor: '#fd7c59' }} onClick={() => handleResult()}>TESTAR SORTEIO </button>
                   </div>
                 </div>
               </div>
             </div>
+            {!result ? '' : <p className="text-center">{result}</p>}
           </div>
         </div>
       </div>
